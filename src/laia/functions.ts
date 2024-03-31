fd29d09efad0c6639cb9b93e2b2bdd3c5b6080c1
albieta <alba.roma@estudiantat.upc.edu>
2024-03-31 22:19:56 +0200

    start

---
+++ src/laia/functions.ts
@@ -0,0 +1,22 @@
+export const functions = {
+    "project_files_architecture": {
+        "name": "",
+        "description": ""
+    },
+    "read_file": {
+        "name": "",
+        "description": ""
+    },
+    "edit_file": {
+        "name": "",
+        "description": ""
+    },
+    "create_file": {
+        "name": "",
+        "description": ""
+    },
+    "delete_file": {
+        "name": "",
+        "description": ""
+    }
+}
